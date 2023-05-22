21d0c61eb48fe46fac393aeb440b5ac145e42a27
Nemanja Milivojevic <nemanja.milivojevic.991@gmail.com>
2023-05-22 17:28:15 +0200

    Dependency Injection without Spring

---
+++ di/src/test/java/nemanja/springframework/di/controller/SetterInjectedControllerTest.java
@@ -0,0 +1,21 @@
+package nemanja.springframework.di.controller;
+
+import nemanja.springframework.di.service.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SetterInjectedControllerTest {
+    SetterInjectedController controller;
+    @BeforeEach
+    void setUp() {
+        controller = new SetterInjectedController();
+        controller.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(controller.getGreeting());
+    }
+}
